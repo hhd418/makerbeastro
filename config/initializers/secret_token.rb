b73fd381504808a9809b92c645d248120045963c
hhd418 <hhd418@gmail.com>
2013-10-12 21:08:05 -0500

    Initial commit with our gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makerbeastro::Application.config.secret_key_base = '118ee7be92fe576d274500bd9705fb38b104008186c11079e7a51b7183014433848b5f98c1e0b3e104b197c0d1dac8d81303886760909253666d2a36d825055d'
